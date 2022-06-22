738e0b6e8afdf8b7c8ad63cdd6d853cacaf8f2f7
A Krishnaprasad <krishna.arakkal@gmail.com>
2022-06-22 14:17:55 +0530

    completed BasicMaths4

--- Day4/BasicMaths4.py
+++ Day4/BasicMaths4.py
@@ -1,1 +1,6 @@
-print('hi')
+m,n = int(input()),int(input())
+min_num = min(m,n)
+for i in range(min_num, 0, -1):
+    if(m%i == 0 and n%i == 0):
+        gcd = i; break
+print("HCF={} LCM={}".format(gcd,m*n//gcd))
